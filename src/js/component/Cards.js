76efc2965fcdc6577dadf0e98e435254f9308043
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-03-12 02:16:10 +0000

    cardswithdiv

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export function Cards() {
-    return (<Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-    </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-    </Card>);
+    return (
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    );
 }
