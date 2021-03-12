699b701ac6f1c4eecbecadee3a1263177ce1efce
LucasDM1 <lucas19651@hotmail.com>
2021-03-12 02:41:14 +0000

    finalCards

--- src/js/component/Cards.js
+++ src/js/component/Cards.js
@@ -9,7 +9,7 @@ export function Cards() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card content.
+                        make up the bulk of the card's content.
 					</p>
                     <a href="#" className="btn btn-primary">
                         Go somewhere
@@ -22,7 +22,7 @@ export function Cards() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card content.
+                        make up the bulk of the card's content.
 					</p>
                     <a href="#" className="btn btn-primary">
                         Go somewhere
@@ -35,7 +35,7 @@ export function Cards() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card content.
+                        make up the bulk of the card's content.
 					</p>
                     <a href="#" className="btn btn-primary">
                         Go somewhere
@@ -48,7 +48,7 @@ export function Cards() {
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">
                         Some quick example text to build on the card title and
-                        make up the bulk of the card content.
+                        make up the bulk of the card's content.
 					</p>
                     <a href="#" className="btn btn-primary">
                         Go somewhere
